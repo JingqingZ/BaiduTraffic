20a0f766bf8a869d601fada9682466c8e533a15b
Jingqing  Zhang <jingqing.zhang15@imperial.ac.uk>
2018-01-20 14:53:44 +0000

    pagerank

--- src/playground.py
+++ src/playground.py
@@ -77,7 +77,7 @@ def roadnet_extraction():
 
     print("Getting links ...")
     # eventsetfilename = datapath + "event_link_set_beijing"
-    eventsetfilename = datapath + "event_link_set_beijing"
+    eventsetfilename = datapath + "event_link_set_beijing_1km"
     eventsetfile = open(eventsetfilename, "r")
     event_road = dict()
     linklist = list()
@@ -103,58 +103,55 @@ def roadnet_extraction():
                 linklist.append((njd, nid))
     '''
 
+    def deep_next(gid, depth):
+        if gid not in node_dict:
+            return list()
+        allnext = list()
+        for d in range(depth):
+            if d == 0:
+                allnext.append(node_dict[gid].next)
+            else:
+                allnext.append(list())
+                for pid in allnext[d - 1]:
+                    if pid in node_dict:
+                        allnext[d] += node_dict[pid].next
+        return allnext
+
+    def deep_previous(gid, depth):
+        if gid not in node_dict:
+            return list()
+        allprev = list()
+        for d in range(depth):
+            if d == 0:
+                allprev.append(node_dict[gid].previous)
+            else:
+                allprev.append(list())
+                for pid in allprev[d - 1]:
+                    if pid in node_dict:
+                        allprev[d] += node_dict[pid].previous
+        return allprev
+
     bar = progressbar.ProgressBar(max_value=1151)
     for iter, line in enumerate(eventsetfile):
         bar.update(iter)
         nodeids = line.replace("\n", "").split("\t")
 
         clink = list()
+        deepprev = dict()
+        deepnext = dict()
         # all_neighbour = list()
         for nid in nodeids:
             # all_neighbour += node_dict[nid].next + node_dict[nid].previous
+            deepprev[nid] = deep_previous(nid, 2)
+            deepnext[nid] = deep_next(nid, 2)
+
+        for nid in nodeids:
             for njd in nodeids:
                 if nid == njd:
                     continue
 
-                def deep_next(gid, depth):
-                    if gid not in node_dict:
-                        return list()
-                    allnext = list()
-                    for d in range(depth):
-                        if d == 0:
-                            allnext.append(node_dict[gid].next)
-                        else:
-                            allnext.append(list())
-                            for pid in allnext[d - 1]:
-                                if pid in node_dict:
-                                    allnext[d] += node_dict[pid].next
-                    return allnext
-
-                def deep_previous(gid, depth):
-                    if gid not in node_dict:
-                        return list()
-                    allprev = list()
-                    for d in range(depth):
-                        if d == 0:
-                            allprev.append(node_dict[gid].previous)
-                        else:
-                            allprev.append(list())
-                            for pid in allprev[d - 1]:
-                                if pid in node_dict:
-                                    allprev[d] += node_dict[pid].previous
-                    return allprev
-
-                '''
-                print(nid)
-                print(node_dict[nid].previous)
-                print(node_dict[nid].next)
-                print(deep_previous(nid, 3))
-                print(deep_next(njd, 3))
-                exit()
-                '''
-
                 foundflag = False
-                for idx, dn in enumerate(deep_next(nid, 8)):
+                for idx, dn in enumerate(deepnext[nid]):
                     if njd in dn:
                         clink.append((nid, njd, idx))
                         foundflag = True
@@ -163,7 +160,7 @@ def roadnet_extraction():
                 if foundflag:
                     continue
 
-                for idx, dn in enumerate(deep_previous(njd, 8)):
+                for idx, dn in enumerate(deepprev[njd]):
                     if nid in dn:
                         clink.append((nid, njd, idx))
                         foundflag = True
@@ -192,7 +189,7 @@ def roadnet_extraction():
     eventsetfile.close()
 
     print("Saving ... ")
-    resultfilename = resultspath + "event_link_set_beijing_link.txt"
+    resultfilename = resultspath + "event_link_set_beijing_link_1km.txt"
     resultfile = open(resultfilename, "w")
     for link in linklist:
         resultfile.write(str(link))
@@ -200,26 +197,76 @@ def roadnet_extraction():
     resultfile.close()
 
 def draw_roadnet():
-    roadnetfilename = resultspath + "event_link_set_beijing_link.txt"
+    roadnetfilename = resultspath + "event_link_set_beijing_link_1km.txt"
     roadnetfile = open(roadnetfilename, "r")
 
+    # prfilename = resultspath + "pagerank_1km.txt"
+    # prfile = open(prfilename, "w")
     bar = progressbar.ProgressBar(max_value=1151)
     for iter, line in enumerate(roadnetfile):
         bar.update(iter)
 
         content = eval(line)
 
-        graph = nx.Graph()
+        graph = nx.DiGraph()
+        linkdict = dict()
         for link in content:
             if link[2] == 1:
                 graph.add_nodes_from(link[:-1])
                 graph.add_edge(link[0], link[1])
+                if link[0] not in linkdict:
+                    linkdict[link[0]] = list()
+                linkdict[link[0]].append(link[1])
+        prnodes = nx.pagerank(graph, max_iter=10000, tol=1e-8)
+        # prfile.write(str(prnodes))
+        # prfile.write("\n")
+
+        '''
+        nsize = list()
+        for node in graph.nodes:
+            nsize.append(prnodes[node])
+        nsize = np.array(nsize)
+        nsize *= 8 / np.max(nsize)
+        nsize = np.exp(nsize)
+        nsize *= 600 / np.max(nsize)
         # nx.draw(graph, options)
-        nx.draw(graph, node_size=50)
-        plt.savefig(resultspath + "figs/roadnet_%d.png" % iter)
-        plt.clf()
+        nx.draw(graph, node_size=nsize)
+        # nx.draw(graph, node_size=20)
+        '''
+
+        import operator
+        sortedlist = list()
+        for node, value in sorted(prnodes.items(), key=operator.itemgetter(1), reverse=True):
+            sortedlist.append((node, value))
+        # prfile.write(str(sortedlist))
+        # prfile.write("\n")
+
+        selected_graph = nx.DiGraph()
+        selected_nsize = list()
+        for i in range(200):
+            selected_graph.add_node(sortedlist[i][0])
+            selected_nsize.append(sortedlist[i][1])
+        for ni in selected_graph.nodes:
+            for nj in selected_graph.nodes:
+                if ni == nj:
+                    continue
+                if ni in linkdict and nj in linkdict[ni]:
+                    selected_graph.add_edge(ni, nj)
+
+
+        selected_nsize = np.array(selected_nsize)
+        selected_nsize *= 8 / np.max(selected_nsize)
+        selected_nsize = np.exp(selected_nsize)
+        selected_nsize *= 600 / np.max(selected_nsize)
+
+        nx.draw(selected_graph, node_size=selected_nsize)
+        plt.show()
+        # plt.savefig(resultspath + "figs/roadnet_set_1km/roadnet_%d.png" % iter)
+        # plt.clf()
+        exit()
 
     roadnetfile.close()
+    # prfile.close()
 
 
 if __name__ == "__main__":
